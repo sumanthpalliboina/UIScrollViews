a19da3a822466ad4fd98c896e97ff07f2c729b9a
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-04-05 14:49:21 +0530

    Initial Commit

---
+++ UIScrollViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScrollViews
+//
+//  Created by Palliboina on 05/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
