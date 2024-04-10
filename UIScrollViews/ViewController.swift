52d7b9b85ccc7cf21b89db4ac9a16b7309a4d1bb
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-04-10 11:37:36 +0530

    scroll images with dots of page control in rotation of landscape also

--- UIScrollViews/ViewController.swift
+++ UIScrollViews/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     var mainScroll:UIScrollView!
     var page:Int = 0
     var imageViews:[UIImageView] = []
+    var rotating:Bool = false
     
     
     override func viewDidLoad() {
@@ -45,22 +46,24 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         let imgHeight = imageView.frame.size.height*/
         
         mainScroll = UIScrollView(frame: .zero)
-        mainScroll.contentSize = CGSize(width: scrollWidth * CGFloat(images.count), height: scrollHeight)
+        ///mainScroll.contentSize = CGSize(width: scrollWidth * CGFloat(images.count), height: scrollHeight)
         //to show with status bar occupied
         mainScroll.contentInsetAdjustmentBehavior = .never
         mainScroll.isPagingEnabled = true
         mainScroll.delegate = self
         
-        var posX:CGFloat = 0
+        
         for img in images {
-            let childScroll = UIScrollView(frame: CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight))
-            childScroll.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
+            ///let childScroll = UIScrollView(frame: CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight))
+            let childScroll = UIScrollView(frame: .zero)
+            ///childScroll.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
             childScroll.contentInsetAdjustmentBehavior = .never
             childScroll.minimumZoomScale = 1.0
             childScroll.maximumZoomScale = 4.0
             childScroll.delegate = self
             
-            let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: scrollWidth, height: scrollHeight))
+            ///let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: scrollWidth, height: scrollHeight))
+            let imgView = UIImageView(frame: .zero)
             imgView.image = UIImage(named: img)
             imgView.contentMode = .scaleAspectFill
             imgView.clipsToBounds = true
@@ -68,7 +71,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             
             childScroll.addSubview(imgView)
             mainScroll.addSubview(childScroll)
-            posX = posX + scrollWidth
+            
         }
         /*let teddyImg = UIImageView(image:UIImage(named:"teddy"))
         teddyImg.translatesAutoresizingMaskIntoConstraints = false
@@ -89,8 +92,26 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         stackView.addArrangedSubview(mainScroll)
         
+        updateSize()
         
+    }
+    
+    func updateSize(){
+        let scrollWidth = stackView.frame.size.width
+        let scrollHeight = stackView.frame.size.height
         
+        var posX:CGFloat = 0
+        for imgView in imageViews{
+            let scroll = imgView.superview as! UIScrollView
+            scroll.frame = CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight)
+            scroll.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
+            imgView.frame = CGRect(x: 0, y: 0, width: scrollWidth, height: scrollHeight)
+            posX = posX + scrollWidth
+        }
+        mainScroll.contentSize = CGSize(width: scrollWidth * CGFloat(imageViews.count), height: scrollHeight)
+        
+        let currentScrollView = imageViews[page].superview as! UIScrollView
+        mainScroll.contentOffset = CGPoint(x: currentScrollView.frame.origin.x, y: 0)
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -98,17 +119,29 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let getPage = round(mainScroll.contentOffset.x / (stackView.frame.size.width))
-        let currentPage = Int(getPage)
-        if currentPage != page{
-            let scroll = imageViews[page].superview as! UIScrollView
-            scroll.setZoomScale(1.0, animated: true)
-            page = Int(currentPage)
-            pageControl.currentPage = page
+        if !rotating {
+            let getPage = round(mainScroll.contentOffset.x / (stackView.frame.size.width))
+            let currentPage = Int(getPage)
+            if currentPage != page{
+                let scroll = imageViews[page].superview as! UIScrollView
+                scroll.setZoomScale(1.0, animated: true)
+                page = Int(currentPage)
+                pageControl.currentPage = page
+            }
         }
     }
     
-    
+    override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        print("rotated")
+        rotating = true
+        coordinator.animate(alongsideTransition: nil, completion: {(context:UIViewControllerTransitionCoordinatorContext!) in
+            let scroll = self.imageViews[self.page].superview as! UIScrollView
+            scroll.setZoomScale(1.0, animated: true)
+            self.updateSize()
+            self.rotating = false
+        })
+    }
 
 
 }
